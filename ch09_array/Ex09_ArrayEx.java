796664871d39b68968b4b3f29c4acd39a57c4d5d
haenocklee <haenock7706@naver.com>
2023-12-11 14:26:53 +0900

    OOP

--- ch09_array/Ex09_ArrayEx.java
+++ ch09_array/Ex09_ArrayEx.java
@@ -6,7 +6,6 @@ public class Ex09_ArrayEx {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] nosA = null;//학생의 점수 배열 1.2.3...
-
         boolean run = true;
 
         while (run) {
@@ -19,24 +18,19 @@ public class Ex09_ArrayEx {
             if (select == 1) {//학생수 입력
                 System.out.print("학생수> ");
                 int nos = scanner.nextInt();
-                if(nos > 0) {
-                    nosA = new int[nos];
-                }else{
-                    System.out.println("잘못된 입력방식 입니다");
-                }
+                nosA = new int[nos];
 
 
-            }else if (select == 2) {//점수 입력
+            } else if (select == 2) {//점수 입력
                 for (int i = 0; i < nosA.length; i++) {
-                    System.out.print(i+1+"번 학생의 점수: ");
+                    System.out.print(i + 1 + "번 학생의 점수: ");
                     nosA[i] = scanner.nextInt();
-
-                    }
                 }
+
 
             } else if (select == 3) {//점수 목록
                 for (int i = 0; i < nosA.length; i++) {
-                    System.out.println(i+1+"번 학생의 점수: " + nosA[i]);
+                    System.out.println(i + 1 + "번 학생의 점수: " + nosA[i]);
 
                 }
 
@@ -46,22 +40,22 @@ public class Ex09_ArrayEx {
                 int avgp = 0; //평균점수합
                 int temp = 0;
                 for (int i = 0; i < nosA.length; i++) {
-                    if(max < nosA[i]){
+                    if (max < nosA[i]) {
                         max = nosA[i];
                     }
                     avgp = avgp + nosA[i];
                 }
                 System.out.println("최고점수: " + max);
-                System.out.println("평균점수: " + (double)(avgp / nosA.length));
+                System.out.println("평균점수: " + (double) avgp / nosA.length);
                 for (int i = 0; i < nosA.length; i++) {
-                    for (int j = i+1; j < nosA.length; j++) {
-                        if(nosA[i] < nosA[j]){
+                    for (int j = i + 1; j < nosA.length; j++) {
+                        if (nosA[i] < nosA[j]) {
                             temp = nosA[i];
                             nosA[i] = nosA[j];
                             nosA[j] = temp;
                         }
                     }
-                    System.out.println((i+1) + "등: " + nosA[i]);
+                    System.out.println((i + 1) + "등: " + nosA[i]);
                 }
 
 
