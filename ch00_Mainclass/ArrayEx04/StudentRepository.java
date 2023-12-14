8013a7cc94c5adedebdbf386946a3e23ba073cc6
haenocklee <haenock7706@naver.com>
2023-12-14 16:46:44 +0900

    OOP객체지향프로그래밍_Class역할 분류

---
+++ ch00_Mainclass/ArrayEx04/StudentRepository.java
@@ -0,0 +1,10 @@
+package ch00_Mainclass.ArrayEx04;
+
+public class StudentRepository {
+    public void method1(){
+        System.out.println("StudentRepository.method1_1");
+    }
+    public void method2(String str1){
+        System.out.println(str1);
+    }
+}
---
+++ ch00_Mainclass/ArrayEx04/StudentService.java
@@ -0,0 +1,17 @@
+package ch00_Mainclass.ArrayEx04;
+
+public class StudentService {
+    StudentRepository studentRepository = new StudentRepository();
+    public void method1 (){
+        System.out.println("StudentService.method1");
+        studentRepository.method1();
+    }
+    public void method2 (){
+        System.out.println("StudentService.method2");
+        String str1 = "파이팅!";
+        studentRepository.method2(str1);
+    }
+    public void method3 (){
+        System.out.println("StudentService.method3");
+    }
+}
